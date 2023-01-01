d739d762193e2cbdfe4458e8e1aeade7415114ef
35V LG84 <35vlg84-x4e6b92@e257.fi>
2023-01-01 23:49:47 +0200

    Proof-of-concept: ANTLR with Tackler grammar
    Unicode doesn't work and error reporting is also broken.
    
    Signed-off-by: 35V LG84 <35vlg84-x4e6b92@e257.fi>

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Tackler-rs
 
-This is rusty [Tackler](https://gitlab.com/e257/accounting/tackler)
+This will be rusty [Tackler](https://gitlab.com/e257/accounting/tackler)
 
+## Tackler format and ANTLR parser
+
+See [Parser definition](./src/txn_antlr/readme.adoc) for grammar and lexer.
+
+## Test cases
+
+Simple test-case
+
+    cargo run tests/0001-test.txn
+
+Full format, without unicode characters
+
+    cargo run tests/format-ascii.txn
+
+Full format (doesn't work at the moment)
+
+    cargo run tests/format.txn
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,82 @@
  * limitations under the License.
  *
  */
+use std::env;
+use std::path::Path;
+use std::io;
+use std::error::Error;
+
+
+use std::fs::File;
+use std::io::{BufReader, Read};
+use std::ops::Deref;
+use log::error;
+
+  use antlr_rust::tree::{
+        ParseTree, ParseTreeListener, ParseTreeVisitor, ParseTreeVisitorCompat, ParseTreeWalker,
+        TerminalNode, Tree, VisitChildren, Visitable,
+    };
+
+use antlr_rust::common_token_stream::CommonTokenStream;
+use antlr_rust::token_factory::{CommonTokenFactory};
+use antlr_rust::InputStream;
+
+pub mod txn_antlr;
+
+use txn_antlr::txnparser::TxnParser;
+use txn_antlr::txnlexer::TxnLexer;
+
+use txn_antlr::txnparser::TxnsContext;
+
+
+
+fn txnsFile(path: &Path) -> Result<String, &'static str> {
+    let mut f = File::open(path).unwrap();
+    let mut txns_str = String::new();
+
+    f.read_to_string(&mut txns_str).unwrap();
+
+    let tf = CommonTokenFactory::default();
+    //let mut _lexer = TxnLexer::new_with_token_factory(InputStream::new("V123,V2\nd1,d2\n".into()), &tf);
+
+    let mut _lexer = TxnLexer::new_with_token_factory(InputStream::new(txns_str.as_bytes()), &tf);
+
+    let token_source = CommonTokenStream::new(_lexer);
+    let mut parser = TxnParser::new(token_source);
+
+    let result = parser.txns();
+
+    match result {
+        Ok(_) => {
+            let res_str = result.unwrap().to_string_tree(&*parser);
+            Ok(res_str)
+        },
+        Err(e) => Err("ANTRL error: TODO Errors are not propagating")
+    }
+}
+
 fn main() {
-    println!("Hello, tackler-rs!");
+    let args: Vec<String> = env::args().collect();
+
+    if (args.len() != 2) {
+        eprintln!("Error: Missing input file");
+        eprintln!("Usage: {} <tackler txns-file>", &args[0]);
+        std::process::exit(1);
+    }
+    let txn_file = Path::new(&args[1]);
+
+    print!("Parsing {} ...", txn_file.display());
+    match txnsFile(txn_file) {
+        Ok(parse_tree) => {
+            println!("ok!");
+            println!("Parse tree is:\n{}\n", parse_tree);
+            std::process::exit(0);
+        }
+        Err(err) => {
+            let msg = format!("Error: {}", err);
+            error!("{}", msg);
+            eprintln!("{}", msg);
+            std::process::exit(1);
+        }
+    }
 }
